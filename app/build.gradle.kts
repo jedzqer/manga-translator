bf8a269a79432b60c8dd8996c433ac1aa67a2b5f
jedzqer <jedzouquan@gmail.com>
2026-01-10 23:59:32 +0800

    新增软件图标

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.manga.translate"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
